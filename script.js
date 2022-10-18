fb62bfaabae2fe2649bf546105b14cd06071de70
Nazar Hotko <nazar.hotko@outlook.com>
2022-10-18 16:39:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+function alert(user) {
+    if (user > 9 || user < 0) {
+        return 'You are choosing the wrong number!'
+    }
+}
+
+function generateTarget() {
+    return Math.floor(Math.random() * 10)
+}
+
+function getAbsoluteDistance(number1, number2) {
+    return Math.abs(number1 - number2);
+}
+
+function compareGuesses(user, computer , secret) {
+    let computerDist = getAbsoluteDistance(computer , secret)
+    let userDist = getAbsoluteDistance(user , secret)
+    if (user > 9 || user < 0) {
+        window.alert('You are choosing the wrong number!')
+    } else if (user === secret) {
+        return true;
+    } else if (userDist < computerDist) {
+        return true;
+    } else if (userDist === computerDist) {
+        return true; 
+    } else if (userDist > computerDist) {
+        return false;
+    } 
+    if (user > 9 || user < 0) {
+        return 'You are choosing the wrong number!'
+    }
+}
+
+function updateScore(name) {
+    if (name === 'human') {
+        humanScore = humanScore + 1;
+    } else if (name === 'computer') {
+        computerScore = computerScore + 1;
+    }
+}
+
+function advanceRound() {
+    currentRoundNumber = currentRoundNumber + 1;
+}
+
+console.log(compareGuesses( 10 ,9, 0))
